d417736a9b20204355ada6b5df186ad29e9ec790
TeoCala97 <mateo.osorio@cala-analytics.com>
2022-06-14 16:54:12 -0500

    1

--- Form/urls.py
+++ Form/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.formu, name="formulario"),
+    path('Campaign/', views.formu, name="formulario"),
     path('?ok', views.formu, name="Json"),
 ]
