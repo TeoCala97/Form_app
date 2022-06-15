6c210084e094de4b6a31fa91b3d8b67787b3face
TeoCala97 <mateo.osorio@cala-analytics.com>
2022-06-15 16:14:41 -0500

    json

--- Form/views.py
+++ Form/views.py
@@ -5,8 +5,18 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.files.storage import FileSystemStorage
 import json
-
 from Form.forms import Formulario
+
+
+from google.cloud import pubsub_v1
+from google.cloud import bigquery
+from google.cloud import storage
+# from google.oauth2 import service_account
+
+
+client = bigquery.Client(project='sod-co-bi-sandbox')
+client_storage= storage.Client(project='sod-co-bi-sandbox')
+bucket = client_storage.get_bucket('crm_symphony_cfg_co')
 
 # Create your views here.
 class FormView(HttpRequest):
@@ -18,7 +28,8 @@ class FormView(HttpRequest):
             if formulario.is_valid():
                 formulario.save()
                 formu = json.dumps(request.POST)
-                print(formu)
+                blob = bucket.blob(formu)
+                blob.upload_from_filename('cfg_sp_audiencia_sod_mo_sku.json')
             return redirect(reverse("formulario")+"?ok" )
         return render(request,'Form/form.html',{'form':formulario})
 
