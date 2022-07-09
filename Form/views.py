069626aacd20c61ce43c0683c90df76ca4a15fcb
Mateo Arias <teoarco@gmail.com>
2022-07-09 00:55:48 -0500

    Se realiza el template del formulario de la campaña ( se organiza y comenta el codigo html, se agrega la carpeta static y seagregan estilos). Queda pendiente rcambiar la imagen de la empresa y terminar las sección dos, tres y el botón.

--- Form/views.py
+++ Form/views.py
@@ -10,22 +10,22 @@ class FormView(HttpRequest):
 
     def formu_index(request):
         formulario = Formulario()
-        Campos = Camp()
-        if request.method == 'POST':
-            formulario = Formulario(data=request.POST)
-            if formulario.is_valid():
-                formulario.save()
-                formu = dict(request.POST)
-                formu['key_lecture']=int(1)
-                GCP_gestor.connect_client('sod-co-bi-sandbox','sod-co-bi-sandbox-campanhas')  
-                jsondata_post = 'cfg_audiencia_parametros_campanhas.json'
-                print(formu)
-                GCP_gestor.post_form(jsondata_post, formu)
-                File = GCP_gestor.get_form()
-                print(File)
-                File['key_lecture']=int(0)
-                jsondata_get = 'post_data.json'
-                GCP_gestor.post_form(jsondata_get, File)
-                print(File)
-            return render(request,'Form/camp.html', {'ID': File['Campanha_id'], 'Nombre_C': File['Nombre_campania'], 'N_registros': File['N_registros']})
+        # Acordarme de poner el if
+        request.method == 'POST'
+        #     formulario = Formulario(data=request.POST)
+        #     if formulario.is_valid():
+        #         formulario.save()
+        #         formu = dict(request.POST)
+        #         formu['key_lecture']=int(1)
+        #         GCP_gestor.connect_client('sod-co-bi-sandbox','sod-co-bi-sandbox-campanhas')  
+        #         jsondata_post = 'cfg_audiencia_parametros_campanhas.json'
+        #         print(formu)
+        #         GCP_gestor.post_form(jsondata_post, formu)
+        #         File = GCP_gestor.get_form()
+        #         print(File)
+        #         File['key_lecture']=int(0)
+        #         jsondata_get = 'post_data.json'
+        #         GCP_gestor.post_form(jsondata_get, File)
+        #         print(File)
+        #     return render(request,'Form/camp.html', {'ID': File['Campanha_id'], 'Nombre_C': File['Nombre_campania'], 'N_registros': File['N_registros']})
         return render(request,'Form/form.html',{'form':formulario})
