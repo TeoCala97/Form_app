8e586b7a43573cf881a1f561192624664d17d747
TeoCala97 <mateo.osorio@cala-analytics.com>
2022-08-01 11:04:52 -0500

    ultima version alavaro

--- Form/urls.py
+++ Form/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('campanha/', views.FormView.formu_index, name="formulario"),
+    path('', views.FormView.formu_index, name="formulario"),
     path('campanha/campanha_enviada/', views.FormView.formu_index, name="campaña"),
 ]
